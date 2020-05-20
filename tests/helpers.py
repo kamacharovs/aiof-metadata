3e9fef074d734b3374d23cde7f9b42180cd43153
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-20 15:50:26 -0400

    update unit tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -8,6 +8,7 @@ class HelpersTestSuite(unittest.TestCase):
 
     def test_convert_frequency_daily(self):
         assert aiof.convert_frequency("daily") == 365
+
     def test_convert_frequency_multiple_frequencies(self):
         frequencires = [
             "daily",
@@ -16,6 +17,14 @@ class HelpersTestSuite(unittest.TestCase):
         ]
         for f in frequencires:
             assert aiof.convert_frequency(f) > 0
+
+    def test_convert_frequency_raises_exception(self):
+        with self.assertRaises(Exception): aiof.convert_frequency("test")
+
+
+
+    def test_compound_interest(self):
+        assert True
 
 
 if __name__ == "__main__":
