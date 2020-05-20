b117420a000bf67f89ca3e02a5e0a1a5c10cc0c2
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-20 15:43:14 -0400

    update to use frequencies as dict

--- aiof/helpers.py
+++ aiof/helpers.py
@@ -1,26 +1,16 @@
-_frequency = [
-    "daily",
-    "monthly",
-    "quarterly",
-    "half-year",
-    "yearly"
-]
+_frequency = {
+    "daily": 365,
+    "monthly": 12,
+    "quarterly": 4,
+    "half-year": 6,
+    "yearly": 1
+}
 
 
 def convert_frequency(frequency):
     if frequency not in _frequency:
         raise Exception("frequency must be one of the following: " + ",".join(_frequency))
-
-    if frequency == "daily":
-        frequency_float = 365
-    elif frequency == "monthly":
-        frequency_float = 12
-    elif frequency == "half-year":
-        frequency_float = 6
-    elif frequency == "yearly":
-        frequency_float = 1
-
-    return float(frequency_float)
+    return float(_frequency[frequency])
 
 
 def compound_interest(principal_amount, number_of_years, rate_of_interest, frequency = "yearly"):
