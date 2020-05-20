ee7421af4441ad2bdcc46b2bf76937a6c6aab4d4
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-20 15:43:26 -0400

    update unit tests for convert frequency

--- tests/helpers.py
+++ tests/helpers.py
@@ -8,6 +8,14 @@ class HelpersTestSuite(unittest.TestCase):
 
     def test_convert_frequency_daily(self):
         assert aiof.convert_frequency("daily") == 365
+    def test_convert_frequency_multiple_frequencies(self):
+        frequencires = [
+            "daily",
+            "quarterly",
+            "yearly"
+        ]
+        for f in frequencires:
+            assert aiof.convert_frequency(f) > 0
 
 
 if __name__ == "__main__":
