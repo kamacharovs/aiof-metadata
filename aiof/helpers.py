22f5ff965a06a614faab9ec788a9444045a5e6bc
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-20 14:52:48 -0400

    add frequency and compound interest calculations

---
+++ aiof/helpers.py
@@ -0,0 +1,27 @@
+
+_frequency = [
+    "daily",
+    "monthly",
+    "quarterly",
+    "half-year",
+    "yearly"
+]
+
+
+def convert_frequency(frequency):
+    if frequency not in _frequency:
+        raise Exception("frequency must be one of the following: " + ",".join(_frequency))
+
+    if frequency == "daily":
+        frequency_float = 365
+    elif frequency == "monthly":
+        frequency_float = 12
+    elif frequency == "half-year":
+        frequency_float = 6
+    elif frequency == "yearly":
+        frequency_float = 1
+
+
+def compound_interest(principal_amount, number_of_years, rate_of_interest, frequency = "yearly"):
+    frequency_float = convert_frequency(frequency)
+    return principal_amount * (1 + (rate_of_interest / (100 * frequency_float)) ** (frequency_float * number_of_years))
