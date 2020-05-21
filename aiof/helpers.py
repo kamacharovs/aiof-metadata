ca88a64141ac08c2dc8f3ba3647010559d646a33
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-21 09:53:47 -0400

    add loan calculator

--- aiof/helpers.py
+++ aiof/helpers.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy_financial as npf
+
+
 _frequency = {
     "daily": 365,
     "monthly": 12,
@@ -13,12 +17,17 @@ def convert_frequency(frequency):
     return float(_frequency[frequency])
 
 
+def to_percentage(number):
+    if number < 0 or number > 100:
+        raise Exception("number can't be less than 0 or bigger than 100")
+    return float(number) / 100
+
+
 def compound_interest_calc(principal_amount, number_of_years, rate_of_interest, frequency = "yearly"):
     frequency_float = convert_frequency(frequency)
     return principal_amount * (pow(1 + ((rate_of_interest / 100) / frequency_float), frequency_float * number_of_years))
 
 
-def to_percentage(number):
-    if number < 0 or number > 100:
-        raise Exception("number can't be less than 0 or bigger than 100")
-    return float(number) / 100
+def loan_payments_calc(loan_amount, number_of_years, rate_of_interest, frequency = "yearly"):
+    car_payments = npf.pmt(rate = to_percentage(rate_of_interest), nper = number_of_years, pv = -loan_amount)
+    return car_payments / convert_frequency(frequency)
