00d27a594e0b4d0c3f340372e2d8cb679119a621
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-22 14:15:15 -0400

    add mortgage calc

--- aiof/house/core.py
+++ aiof/house/core.py
@@ -1,4 +1,9 @@
+import numpy as np
+
+from aiof.helpers import to_percentage, convert_frequency
 
 
 def mortgage_calc(principal_amount, rate_of_interest, number_of_periods, frequency="monthly"):
-    return ""
+    interest = to_percentage(rate_of_interest)
+    periods = convert_frequency(frequency)
+    return principal_amount * ((pow(interest * (1 + interest), number_of_periods)) / (pow(1 + interest, number_of_periods) - 1))
