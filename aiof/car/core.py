51cde749377fd5f93d1ddc551be5a36f62e8879c
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-22 15:40:49 -0400

    update to return calculation and more unit tests

--- aiof/car/core.py
+++ aiof/car/core.py
@@ -1,9 +1,5 @@
 from aiof.helpers import *
 
 
-def test_helpers():
-    to_percentage(10)
-
-
-def test():
-    return "car"
+def loan_calc(principal_amount, rate_of_interest, number_of_months):
+    return loan_payments_calc(principal_amount, number_of_months / 12, rate_of_interest, frequency="monthly")
--- tests/car.py
+++ tests/car.py
@@ -7,5 +7,9 @@ from aiof.car.core import *
 class CarTestCase(unittest.TestCase):
     """Car unit tests"""
 
-    def test_truth(self):
-        assert test() == "car"
+    def test_car_loan_calc_30_months(self):
+        assert loan_calc(25000, 3.11, 30) > 800
+    def test_car_loan_calc_12_months(self):
+        assert loan_calc(25000, 3.11, 12) > 1600
+    def test_car_loan_calc_1_month(self):
+        assert loan_calc(25000, 3.11, 1) > 25000
