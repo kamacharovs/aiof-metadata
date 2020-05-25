3306a0e7404278d88db342c811eebf3fa72c3c9f
dkamacharov19 <dkamacharov@gmail.com>
2020-05-25 10:34:29 -0400

    added def test()

--- aiof/core.py
+++ aiof/core.py
@@ -11,3 +11,6 @@ def loan_payments_calc_as_table(content_as_json, frequency="monthly"):
     rate_of_interest = content_as_json["rateOfInterest"]
     #TODO: add input (parameter) checks
     return helpers.loan_payments_calc_as_table(loan_amount, number_of_years, rate_of_interest, frequency)
+
+
+def test()
