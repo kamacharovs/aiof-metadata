a7739f42290fe5fac28c2ac3de7f89d3675e001f
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-25 00:05:53 -0400

    update param name to be more understandable

--- aiof/core.py
+++ aiof/core.py
@@ -5,9 +5,9 @@ def frequencies():
     return list(helpers._frequency.keys())
 
 
-def loan_payments_calc_as_table(content, frequency="monthly"):
-    loan_amount = content["loanAmount"]
-    number_of_years = content["numberOfYears"]
-    rate_of_interest = content["rateOfInterest"]
+def loan_payments_calc_as_table(content_as_json, frequency="monthly"):
+    loan_amount = content_as_json["loanAmount"]
+    number_of_years = content_as_json["numberOfYears"]
+    rate_of_interest = content_as_json["rateOfInterest"]
     #TODO: add input (parameter) checks
     return helpers.loan_payments_calc_as_table(loan_amount, number_of_years, rate_of_interest, frequency)
