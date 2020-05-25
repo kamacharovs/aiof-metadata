7cda58a4d176207f18b11ff898096e753d2b5642
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-25 00:05:32 -0400

    update to accept request content as json

--- aiof/core.py
+++ aiof/core.py
@@ -5,6 +5,9 @@ def frequencies():
     return list(helpers._frequency.keys())
 
 
-def loan_payments_calc_as_table(loan_amount, number_of_years, rate_of_interest, frequency="monthly"):
+def loan_payments_calc_as_table(content, frequency="monthly"):
+    loan_amount = content["loanAmount"]
+    number_of_years = content["numberOfYears"]
+    rate_of_interest = content["rateOfInterest"]
     #TODO: add input (parameter) checks
     return helpers.loan_payments_calc_as_table(loan_amount, number_of_years, rate_of_interest, frequency)
