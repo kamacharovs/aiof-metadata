c04d2c792cbce74417e99a4cd69ac2ee4f90d119
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-24 23:53:58 -0400

    update to add loan payments calc to core

--- aiof/core.py
+++ aiof/core.py
@@ -3,3 +3,8 @@ import aiof.helpers as helpers
 
 def frequencies():
     return list(helpers._frequency.keys())
+
+
+def loan_payments_calc_as_table(loan_amount, number_of_years, rate_of_interest, frequency="monthly"):
+    #TODO: add input (parameter) checks
+    return helpers.loan_payments_calc_as_table(loan_amount, number_of_years, rate_of_interest, frequency)
