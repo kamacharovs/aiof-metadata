b41270c575232ef9ae2f9ae00834f991f0e6b92c
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-25 23:11:34 -0400

    update loan payments table to return as json

--- aiof/core.py
+++ aiof/core.py
@@ -1,3 +1,6 @@
+import json
+import pandas as pd
+
 import aiof.helpers as helpers
 
 
@@ -17,7 +20,7 @@ def loan_payments_calc_as_table(content_as_json, frequency="monthly"):
     elif rate_of_interest < 0 or rate_of_interest > 100:
         raise Exception("incorrect input. rate of interest must be bigger than 0 and less than 100")
 
-    return helpers.loan_payments_calc_as_table(loan_amount, number_of_years, rate_of_interest, frequency)
+    return json.loads((helpers.loan_payments_calc_as_table(loan_amount, number_of_years, rate_of_interest, frequency)).to_json(orient="records"))
 
 
 def test():
