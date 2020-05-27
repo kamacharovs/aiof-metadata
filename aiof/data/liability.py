35a60d4ed6e9192a860fc0e10aaa9e26658dbf1b
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-27 09:19:20 -0400

    add liability

---
+++ aiof/data/liability.py
@@ -0,0 +1,25 @@
+# Anything that you are paying interest on:
+#   - Credit card
+#   - Student loans
+#   - Car loan
+#   - Mortgage
+#   - House renovation
+#   - RV
+#   - Any personal loan
+#   - Etc.
+class Liability:
+    _types = [
+        "credit card",
+        "car loan",
+        "mortgage",
+        "house renovation",
+        "rv",
+        "personal loan",
+        "loan",
+        "other"
+    ]
+
+    def __init__(self, name, type, value):
+        self.name = name
+        self.type = type if type in _types else "other"
+        self.value = value
