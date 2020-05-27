b44b5fb675d7a3250af9ddc092b76dd82a7f24c9
Georgi Kamacharov <gkamacharov@gmail.com>
2020-05-27 09:37:03 -0400

    add goal

---
+++ aiof/data/goal.py
@@ -0,0 +1,13 @@
+# Financial Goals
+#   - Mandatory short-term goals
+
+class Goal:
+    _types = [
+        "short-term",
+        "long-term"
+    ]
+
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type if type in _types else "other"
+        self.savings = False
