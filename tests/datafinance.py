48f919cba715d0ce8d03b43a6f53f5fc1ae7a30a
Georgi Kamacharov <gkamacharov@gmail.com>
2020-06-05 11:48:51 -0400

    add unit tests

--- tests/datafinance.py
+++ tests/datafinance.py
@@ -11,8 +11,7 @@ from aiof.data.finance import Finance
 class FinanceTestCase(unittest.TestCase):
     """Finance unit tests"""
 
-
-    def test_finance_total_assets_value(self):
+    def get_test_finance(self):
         assets = [
             Asset("nissan versa", "car", 14500),
             Asset("house", "house", 250000)
@@ -25,9 +24,20 @@ class FinanceTestCase(unittest.TestCase):
             Goal("savings", "short-term")
         ]
 
-        finance = Finance(1, assets, liabilities, goals)
+        return Finance(1, assets, liabilities, goals)
+
+    def test_finance_total_assets_value(self):
+        finance = self.get_test_finance()
 
         assert finance.get_total_assets_value() == 264500
         assert finance.get_distinct_assets_types() == ["car", "house"]
         assert finance.get_total_liabilities_value() == 150878
         assert finance.get_total_balance_sheet() > 0
+
+    def test_finance_total_stats(self):
+        finance = self.get_test_finance()
+        finance_stats = finance.get_total_finance_stats()
+        
+        assert finance_stats["assets"] > 0
+        assert finance_stats["liabilities"] > 0
+        assert finance_stats["balanceSheet"] > 0
