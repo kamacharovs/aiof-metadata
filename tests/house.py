3a0c338531d18e6a987b8f747864c4797b167bfe
Georgi Kamacharov <gkamacharov@gmail.com>
2020-06-08 08:56:23 -0400

    update unit tests for house core

--- tests/house.py
+++ tests/house.py
@@ -8,4 +8,8 @@ class HouseTestCase(unittest.TestCase):
     """House unit tests"""
 
     def test_mortgage_calc_yearly(self):
-        assert mortgage_calc(250000, 3.25, 30) > 0
+        assert house_mortgage_calc(250000, 3.25, 30) > 0
+
+
+    def test_house_future_value_calc(self):
+        assert round(house_future_value_calc(1000, 2, 5), 2) == 5204.04
