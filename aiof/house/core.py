f2ce49c36eddf982aacb7ee86d27070d6cf3d917
Georgi Kamacharov <gkamacharov@gmail.com>
2020-06-08 08:55:37 -0400

    update to add future value calc

--- aiof/house/core.py
+++ aiof/house/core.py
@@ -1,11 +1,16 @@
 import numpy as np
+import pandas as pd
 
-from aiof.helpers import to_percentage, convert_frequency
+from aiof.helpers import to_percentage, convert_frequency, future_value_calc
 
 
-def mortgage_calc(principal_amount, rate_of_interest, number_of_periods, frequency="yearly"):
+def house_mortgage_calc(principal_amount, rate_of_interest, number_of_periods, frequency="yearly"):
     interest = to_percentage(rate_of_interest)
     periods = convert_frequency(frequency) * number_of_periods
     numerator = interest * pow((1 + interest), periods)
     denominator = pow(1 + interest, periods) - 1
     return principal_amount * (numerator / denominator)
+
+
+def house_future_value_calc(periodic_payment, rate_of_interest, number_of_years, frequency="yearly"):
+    return future_value_calc(periodic_payment, rate_of_interest, number_of_years, frequency)
