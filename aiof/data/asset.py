6dc87edf219afef87f111382cf5d4bf5bff42135
Georgi Kamacharov <gkamacharov@gmail.com>
2020-09-29 00:12:19 -0400

    update to move to defaults

--- aiof/data/asset.py
+++ aiof/data/asset.py
@@ -6,6 +6,14 @@ from typing import Optional
 # Include anything purchased with cash or with a loan – car, house, boat, investment property, etc. 
 # Mainly interested in larger purchases (i.e., don’t care if financing a microwave)
 
+_default_interest = 8
+_default_hysInterest = 1.75
+_default_years = 25
+_default_frequency = 12
+_default_investmentFees = 0
+_default_taxDrag = 0
+_default_contribution = 500
+
 
 class Asset:
     _types = [
@@ -29,13 +37,13 @@ class ComparableAsset(BaseModel):
     type: Optional[str]
     value: float
 
-    interest: Optional[float] = 8                               # Interest
-    hysInterest: Optional[float] = 1.75                         # High yield savings interest
-    years: Optional[int] = 25                                   # Number of years to compound
-    frequency: Optional[int] = 12                               # Frequency of compounding, 12 = monthly
-    investmentFees: Optional[float] = 0                         # Investment fees, if any   (reduced from interest)
-    taxDrag: Optional[float] = 0                                # Tax drag, if any          (reduced from interest)
-    contribution: Optional[float] = 500                         # Contribution per frequency
+    interest: Optional[float] = _default_interest               # Interest
+    hysInterest: Optional[float] = _default_hysInterest         # High yield savings interest
+    years: Optional[int] = _default_years                       # Number of years to compound
+    frequency: Optional[int] = _default_frequency               # Frequency of compounding, 12 = monthly
+    investmentFees: Optional[float] = _default_investmentFees   # Investment fees, if any   (reduced from interest)
+    taxDrag: Optional[float] = _default_taxDrag                 # Tax drag, if any          (reduced from interest)
+    contribution: Optional[float] = _default_contribution       # Contribution per frequency
 
     marketValue: Optional[float] = None                         # What if asset's value is invested in the market at [interest]% return?
     marketBeginValue: Optional[float] = None                       # When the compounding is at the beginning
@@ -54,10 +62,10 @@ class ComparableAsset(BaseModel):
     hysWithContributionValueBreakdown: Optional[list] = None        # Breakdown of each year
 
     def init_values(self):
-        self.interest = self.interest if self.interest is not None else 8
-        self.hysInterest = self.hysInterest if self.hysInterest is not None else 1.75
-        self.years = self.years if self.years is not None else 25
-        self.frequency = self.frequency if self.frequency is not None else 12
-        self.investmentFees = self.investmentFees if self.investmentFees is not None else 0
-        self.taxDrag = self.taxDrag if self.taxDrag is not None else 0
-        self.contribution = self.contribution if self.contribution is not None else 500
+        self.interest = self.interest if self.interest is not None else _default_interest
+        self.hysInterest = self.hysInterest if self.hysInterest is not None else _default_hysInterest
+        self.years = self.years if self.years is not None else _default_years
+        self.frequency = self.frequency if self.frequency is not None else _default_frequency
+        self.investmentFees = self.investmentFees if self.investmentFees is not None else _default_investmentFees
+        self.taxDrag = self.taxDrag if self.taxDrag is not None else _default_taxDrag
+        self.contribution = self.contribution if self.contribution is not None else _default_contribution
