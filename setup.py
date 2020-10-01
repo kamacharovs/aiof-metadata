ed8c2a09afdb83b62ac93756c1123e496af90109
Georgi Kamacharov <gkamacharov@gmail.com>
2020-09-30 20:25:37 -0400

    update setup README read

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 
-with open("README.md") as f:
-    readme = f.read()
+#with open("README.md") as f:
+#    readme = f.read()
 
 with open("LICENSE") as f:
     license = f.read()
@@ -11,7 +11,7 @@ setup(
     name = "aiof-metadata",
     version = "0.1.0",
     description = "All in one finance data crunching backend",
-    long_description = readme,
+    #long_description = readme,
     author = "Georgi Kamacharov",
     author_email = "aiof@email.com",
     url = "https://github.com/gkama/aiof-metadata",
