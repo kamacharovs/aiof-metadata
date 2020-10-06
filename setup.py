05bb7508c851708ae892597157ee7c9166cf78a2
Georgi Kamacharov <gkamacharov@gmail.com>
2020-10-06 15:00:07 -0400

    add if main

--- setup.py
+++ setup.py
@@ -7,14 +7,16 @@ with open("README.md", encoding="utf-8") as f:
 with open("LICENSE", encoding="utf-8") as f:
     license = f.read()
 
-setup(
-    name = "aiof-metadata",
-    version = "0.1.0",
-    description = "All in one finance data crunching backend",
-    long_description = readme,
-    author = "Georgi Kamacharov",
-    author_email = "aiof@email.com",
-    url = "https://github.com/gkama/aiof-metadata",
-    license = license,
-    packages = find_packages(exclude=("tests", "docs"))
-)
+
+if __name__ == "__main__":
+    setup(
+        name = "aiof-metadata",
+        version = "0.1.0",
+        description = "All in one finance data crunching backend",
+        long_description = readme,
+        author = "Georgi Kamacharov",
+        author_email = "aiof@email.com",
+        url = "https://github.com/gkama/aiof-metadata",
+        license = license,
+        packages = find_packages(exclude=("tests", "docs"))
+    )
