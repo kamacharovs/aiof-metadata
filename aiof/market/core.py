359dc4c5c4eafb6dff1cd6eb02483405b494ae76
Georgi Kamacharov <gkamacharov@gmail.com>
2020-10-19 08:55:20 -0400

    add simple get SPY data for a year

--- aiof/market/core.py
+++ aiof/market/core.py
@@ -1,4 +1,28 @@
 import numpy as np
 import pandas as pd
 
-#from aiof.helpers import to_percentage, convert_frequency, loan_payments_calc_as_table, future_value_calc
+import datetime
+import pandas_datareader as pdr
+
+
+def get_spy(
+    date_from: datetime = None,
+    date_to: datetime = None) -> pd.DataFrame:
+    """
+    Get SPY data within a date range
+
+    Parameters
+    ----------
+    `date_to` : datetime or None.
+        date from which to get finance data. defaults to `datetime.datetime.now()`\n
+    `date_from` : datetime or None.
+        date to which to get finance data. defaults to `date_to - datetime.timedelta(days=365)`\n
+    """
+    date_to = date_to if date_to is not None else datetime.datetime.now()
+    date_from = date_from if date_from is not None else date_to - datetime.timedelta(days=365)
+    data  = pdr.get_data_yahoo("spy", date_from, date_to, adjust_price=False)
+    print(data)
+
+
+if __name__ == "__main__":
+    get_spy()
