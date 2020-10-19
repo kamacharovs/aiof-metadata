7b7f0a2ed7578f193baa2777821c0c31e0fae69f
Georgi Kamacharov <gkamacharov@gmail.com>
2020-10-19 14:03:27 -0400

    add test market

---
+++ tests/test_market.py
@@ -0,0 +1,10 @@
+import unittest
+import json
+import datetime
+import pandas as pd
+
+from aiof.market.core import get_spy
+
+
+class MarketTestCase(unittest.TestCase):
+    """Market unit tests"""
